24e7e01548eed74f80956c129744d75e6cde517a
David Gràcia i Llobet <gruay.dgll@gmail.com>
2013-01-17 19:43:55 +0100

    Global
    Una classe per guardar els noms dels atributs, bases de dades i
    col·leccions

---
+++ src/main/java/tfg/Global.java
@@ -0,0 +1,10 @@
+package tfg;
+
+public class Global {
+	public static String DB_TFG = "TFG";
+	public static String C_USERS = "users";
+	public static String A_USER = "username";
+	public static String A_PASSWORD = "password";
+	public static String A_TWITTER = "uTwitter";
+	
+}
