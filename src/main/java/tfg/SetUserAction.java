4a73138509213d0c1e1053d1ecff0f7cc16855fa
David Gràcia i Llobet <gruay.dgll@gmail.com>
2013-01-26 11:00:35 +0100

    SetUserAction
    Falta acabar-la, només creada

---
+++ src/main/java/tfg/SetUserAction.java
@@ -0,0 +1,98 @@
+package tfg;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SetUserAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String hostDB;
+	private int port;
+	private DB db;
+	private String username;
+	private String password;
+	private String uTwitter;
+	
+	public void validate() {
+		if (username.length() == 0) {
+			addFieldError("username", "User Name is required");
+		} 
+		if (password.length() == 0) {
+			addFieldError("password", "Password is required");
+		}
+		if (uTwitter.length() == 0) {
+			addFieldError("uTwitter", "Twitter username is required");
+		}
+	}
+	
+	public String getHostDB() {
+		return hostDB;
+	}
+
+
+	public void setHostDB(String hostDB) {
+		this.hostDB = hostDB;
+	}
+
+
+	public int getPort() {
+		return port;
+	}
+
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getuTwitter() {
+		return uTwitter;
+	}
+
+
+	public void setuTwitter(String uTwitter) {
+		this.uTwitter = uTwitter;
+	}
+	
+
+	@Override
+	public String execute() throws Exception {
+		MongoClient m = new MongoClient(hostDB, port);
+		db = m.getDB(Global.DB_TFG);
+		DBCollection coll = db.getCollection(Global.C_USERS);
+		BasicDBObject q = new BasicDBObject(Global.A_USER, username);
+		if (coll.find(q).hasNext()) {
+			addActionError("Ja existeix un usuari amb aquest nom.");
+			return "error";
+		}
+		coll.insert(new BasicDBObject(Global.A_USER, username).append(Global.A_PASSWORD, password).append(Global.A_TWITTER, uTwitter));
+		return "success";
+	}
+
+}
