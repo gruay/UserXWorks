0805a3b2ad7d91ccae2331b5c27da61c2021af0b
David Gràcia i Llobet <gruay.dgll@gmail.com>
2013-02-06 00:02:42 +0100

    DeleteUserAction
    L'última Action que quedava per fer

---
+++ src/main/java/tfg/DeleteUserAction.java
@@ -0,0 +1,68 @@
+package tfg;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DeleteUserAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String hostDB;
+	private int port;
+	private DB db;
+	private String username;
+	
+	public void validate() {
+		// TODO: falta fer el mètode de validar
+	}
+	
+	public String getHostDB() {
+		return hostDB;
+	}
+
+
+	public void setHostDB(String hostDB) {
+		this.hostDB = hostDB;
+	}
+
+
+	public int getPort() {
+		return port;
+	}
+
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		MongoClient m = new MongoClient(hostDB, port);
+		db = m.getDB(Global.DB_TFG);
+		DBCollection coll = db.getCollection(Global.C_USERS);
+		coll.setObjectClass(User.class);
+		BasicDBObject q = new BasicDBObject(Global.A_USERNAME, username);
+		if (!coll.find(q).hasNext()) {
+			addActionError("No existeix l'usuari");
+			return "error";
+		}
+		coll.remove(q);
+		return "success";
+	}
+
+}
